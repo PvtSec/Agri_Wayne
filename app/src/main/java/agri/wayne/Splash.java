cadd03a6e636b27e0c7c4ff6d19f9b739f425029
PvtSec <akssofts@gmail.com>
2020-04-26 16:07:04 +0530

    Updated some code

--- app/src/main/java/agri/wayne/Splash.java
+++ app/src/main/java/agri/wayne/Splash.java
@@ -1,22 +1,14 @@
 package agri.wayne;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
-import android.widget.RemoteViews;
 import android.widget.Toast;
 import java.util.Objects;
 import agri.wayne.ServerHandler.ServerCommunication;
