e8e90eb1e3bf63ab184ca71963c18b52a1be14fd
PvtSec <akssofts@gmail.com>
2020-04-27 22:50:19 +0530

    Intro Layout

--- app/src/main/java/agri/wayne/IntroActivity.java
+++ app/src/main/java/agri/wayne/IntroActivity.java
@@ -48,12 +48,12 @@ public class IntroActivity extends AppCompatActivity {
         btnAnim = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.button_animation);
         tvSkip = findViewById(R.id.tv_skip);
         final List<agri.wayne.ScreenItem> mList = new ArrayList<>();
-        mList.add(new agri.wayne.ScreenItem("Welcome","We are glad to see you here on Agri Wayne. Swipe right and read through to get a brief idea of how this application support farmers to get most out of their crops.",R.drawable.crop_yield));
-        mList.add(new agri.wayne.ScreenItem("Realtime monitoring","Realtime Weather Condition at your fingertip. Relevant data to that could help keep your crops stable under weather changes",R.drawable.analytics));
-        mList.add(new agri.wayne.ScreenItem("Weather Prediction","Know about upcoming weather",R.drawable.farming));
-        mList.add(new agri.wayne.ScreenItem("Crop Suggestion","Lorem i",R.drawable.paddy));
-        mList.add(new agri.wayne.ScreenItem("Crop Advisory","Lorem i",R.drawable.watering));
-        mList.add(new agri.wayne.ScreenItem("Get Started","Lorem i",R.drawable.get_started));
+        mList.add(new agri.wayne.ScreenItem("Welcome","We are glad to see you here on Agri Wayne. Swipe right and read through to get a brief idea of how this application help farmers to get most out of their crops.",R.drawable.crop_yield));
+        mList.add(new agri.wayne.ScreenItem("Realtime monitoring","Realtime Weather Condition at your fingertip. Relevant data that could help keep your crops stable under weather changes",R.drawable.analytics));
+        mList.add(new agri.wayne.ScreenItem("Weather Prediction","Know about upcoming weather, which will help to take necessary precautions to problems caused by temperature varuations or heavy rainfall.",R.drawable.farming));
+        mList.add(new agri.wayne.ScreenItem("Crop Suggestion","Confused which crop would be suitable under the current Weather? Here we provide suggestion of crops which can be efficiently grown in your area.",R.drawable.paddy));
+        mList.add(new agri.wayne.ScreenItem("Crop Advisory","New to agriculture? Don't worry we provide the perfect crop advisory to maintain your crop through organic farming.",R.drawable.watering));
+        mList.add(new agri.wayne.ScreenItem("Get Started","No more waiting, let's get started. Select your comfortable language for the app content. You can change language in-app options",R.drawable.get_started));
 
         screenPager =findViewById(R.id.screen_viewpager);
         introViewPagerAdapter = new agri.wayne.IntroViewPagerAdapter(this,mList);
